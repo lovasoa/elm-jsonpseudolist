9a6b5df41121f4f97584e84cb044ef40afe585ba
Ophir LOJKINE <pere.jobs@gmail.com>
2017-02-02 01:39:55 +0300

    Handle the case when length < 0

--- src/JsonPseudoList.elm
+++ src/JsonPseudoList.elm
@@ -5,6 +5,10 @@ Elm Json.Decoder for javascript Array-like objects, of the form:
 
     {length:2, 0:value0, 1:value1}
 
+This is usefull for decoding things like
+[DOM `NodeList`s](https://developer.mozilla.org/en/docs/Web/API/NodeList)
+coming from javascript ports.
+
 @docs jsonPseudoList
 -}
 
@@ -20,6 +24,8 @@ import Json.Decode exposing (..)
     You need to use Json.Decode with this module
     >>> import Json.Decode
 
+### Decode an Array-like object with two values
+
     >>> Json.Decode.decodeString
     ...     (jsonPseudoList Json.Decode.string)
     ...     """{
@@ -28,10 +34,39 @@ import Json.Decode exposing (..)
     ...           "1": "world"
     ...         }"""
     Ok ["hello", "world"]
+
+### Try to decode an invalid Array-like object
+The object to decode may contain supplumentary fields, but
+if a value is missing, the decoder will fail
+
+    >>> Json.Decode.decodeString
+    ...     (jsonPseudoList Json.Decode.string)
+    ...     """{
+    ...           "length": 2,
+    ...           "0": "hello"
+    ...         }"""
+    Err "Expecting an object with a field named `1` but instead got: {\"0\":\"hello\",\"length\":2}"
+
+### Fails on negative lengths
+Of course, if the length is less then 0, decoding fails
+
+    >>> Json.Decode.decodeString
+    ...     (jsonPseudoList Json.Decode.string)
+    ...     """{
+    ...           "length": -1
+    ...         }"""
+    Err "I ran into a `fail` decoder: Expecting a positive value for the `length` field"
 -}
 jsonPseudoList : Decoder a -> Decoder (List a)
 jsonPseudoList decoder =
     field "length" int
+        |> andThen
+            (\n ->
+                if n < 0 then
+                    fail "Expecting a positive value for the `length` field"
+                else
+                    succeed n
+            )
         |> andThen (fromLength decoder)
 
 
